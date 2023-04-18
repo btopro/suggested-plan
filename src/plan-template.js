fd330fd9e393711725872be15f38b1a3fc98e4fe
Jps709 <jps7095@psu.edu>
2023-04-18 13:08:57 -0400

    Updoot 0.1.2

---
+++ src/plan-template.js
@@ -0,0 +1,60 @@
+import { LitElement, html, css } from 'lit';
+import './suggested-plan.js';
+
+export class PlanTemplate extends LitElement {
+  static get tag() {
+    return 'plan-template';
+  }
+
+  static get properties() {
+    return {
+      plan: { type: Array },
+    };
+  }
+
+  constructor() {
+    super();
+    this.plan = [];
+    this.updatePlan();
+  }
+
+  updatePlan() {
+    const address = new URL('../api/plan-data.js', import.meta.url).href;
+    fetch(address)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        return [];
+      })
+      .then(data => {
+        this.plan = data;
+      });
+  }
+
+  static get style() {
+    return css``;
+  }
+
+  render() {
+    return html`
+      <div class="wrapper">
+        ${this.plan.map(
+          plan => html`
+            <div class="item">
+              <plan-template
+                weekNum="${plan.weekNum}"
+                compTime="${plan.compTime}"
+                weekTitle="${plan.weekTitle}"
+                weekDescription="${plan.weekDescription}"
+              >
+              </plan-template>
+              <div></div>
+            </div>
+          `
+        )}
+      </div>
+    `;
+  }
+}
+customElements.define(PlanTemplate.tag, PlanTemplate);
--- src/suggested-plan.js
+++ src/suggested-plan.js
@@ -4,6 +4,9 @@ export class SuggestedPlan extends LitElement {
   static get properties() {
     return {
       weekNum: { type: String },
+      compTime: { type: String },
+      weekTitle: { type: String },
+      weekDescription: { type: String },
     };
   }
 
@@ -16,14 +19,25 @@ export class SuggestedPlan extends LitElement {
   constructor() {
     super();
     this.weekNum = '1';
+    this.compTime = '3 Hours';
+    this.weekTitle = 'Misconceptions about happiness';
+    this.weekDescription =
+      'In this module, you will learn what it means to be happy and why pursuing happiness is not a pointless endeavor. Dr. Santos addresses how our minds lie to us and how the science shows that our misconceptions about money, grades, and social media are holding us back from implementing the techniques studied in positive psychology.';
   }
 
   render() {
     return html`
-      <div class="outline">
+      <div class="wrapper">
         <div class="weekly-syllabus">
-          <div class="week">Week ${this.weekNum}</div>
-          <div class="week-info">${this.compTime}</div>
+          <div class="week">
+            <p>Week</p>
+            ${this.weekNum}
+          </div>
+          <div class="week-info">
+            ${this.compTime}
+            <p>${this.weekTitle}</p>
+            <p>${this.weekDescription}</p>
+          </div>
         </div>
       </div>
     `;
